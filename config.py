e2d72b011b3236b0823cc82ddc7c96d99f54839c
koyeb01125 <apnapremiummarket011@gmail.com>
2025-03-04 18:44:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
-    telegram_api_id: int
-    telegram_api_hash: str
-    telegram_bot_token: str
+    telegram_api_id: "22473416"
+    telegram_api_hash: "ca4a166df6f575a22497adcc1451cef6"
+    telegram_bot_token: "7562806732:AAHv28jjwECkKXFvCMUJv9Y68OnqVEar1wk"
     telegram_bot_token_2: str
-    admins: list[int]
+    admins: list["@Adityamajumdaar"]
     limit_spam: int
     admin_to_update_of_payment: int
 
